49193b4815261a3c5dc08b2df7d7b69f78e4486c
theskyworld <2456450687@qq.com>
2024-04-23 14:55:25 +0800

    fix: A PostCSS Plugin was passed as an array but did not provide its configuration

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
-import px2rem from "postcss-px2rem";
-const postcss = px2rem({
-  remUnit: 16,
-});
 const config = {
   plugins: {
     tailwindcss: {},
-    postcss,
   },
 };
 
