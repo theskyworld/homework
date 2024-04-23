f330e5993df7ee1f95201568511629442113add0
theskyworld <2456450687@qq.com>
2024-04-23 12:39:09 +0800

    feat: add all to index.tsx

--- src/components/RightButton.tsx
+++ src/components/RightButton.tsx
@@ -1,8 +1,27 @@
-export default function RightButton() {
+import { useEffect, useRef, useState } from "react"
+
+export default function RightButton({ isDisabled }: { isDisabled: boolean }) {
+  const wrapperElemRef = useRef<HTMLDivElement>(null);
+  const disabledStyle = useRef({
+    cursor: 'not-allowed',
+    opacity: 0
+  })
+
+  const [isHovered, setIsHovered] = useState(false);
+  useEffect(() => {
+    const changeIsHover = (val: boolean) => { setIsHovered(val) };
+    wrapperElemRef.current?.addEventListener('mouseover', changeIsHover.bind(null, true));
+    wrapperElemRef.current?.addEventListener('mouseout', changeIsHover.bind(null, false));
+
+    return () => {
+      wrapperElemRef.current?.removeEventListener('mouseover', changeIsHover.bind(null, true));
+      wrapperElemRef.current?.removeEventListener('mouseout', changeIsHover.bind(null, false));
+    }
+  }, [])
   return (
-    <div className="w-12 h-12 relative cursor-pointer">
-      <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
-        <circle cx="24" cy="24" r="23.6" fill="black" stroke="#676767" stroke-width="0.8" />
+    <div className="w-12 h-12 relative cursor-pointer" ref={wrapperElemRef}>
+      <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none" style={isDisabled ? disabledStyle.current : undefined}>
+        <circle cx="24" cy="24" r="23.6" fill={!isHovered ? 'black' : '#303030'} stroke="#676767" stroke-width="0.8" />
         <path d="M19 16L30 24L19 32" stroke="white" stroke-width="2" stroke-linejoin="bevel" />
       </svg>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,48 @@ import IntroductionCard from "@/components/IntroductionCard";
 import MoonshotCard from "@/components/MoonshotCard";
 import RightButton from "@/components/RightButton";
 import Web3Card from "@/components/Web3Card";
+import { log } from "console";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export default function Home() {
+  const contentWrapperElemRef = useRef<HTMLDivElement>(null);
+  const containerElemRef = useRef<HTMLDivElement>(null);
+  const buttonWrapperElemRef = useRef<HTMLDivElement>(null)
+  const [translateYVal, setTranslateYVal] = useState(0);
+  const [isButtonHide, setIsButtonHide] = useState(false);
+
+  // 判断contentWrapperElemRef和containerElemRef的右边框是否重合，重合则隐藏按钮
+  const isHideButton = useCallback((child: HTMLDivElement, parent: HTMLDivElement, offset: number) => {
+    const childWidth = child.clientWidth;
+    const parentWidth = parent.clientWidth;
+    const childRightBoundary = offset + childWidth;
+    const parentRightBoundary = parentWidth;
+    console.log(childRightBoundary, parentRightBoundary);
+
+    if (childRightBoundary === parentRightBoundary) return true;
+    return false;
+  }, [])
+
+  function handleRightButtonClick() {
+    setTranslateYVal(() => translateYVal - 104);
+  }
+  useEffect(() => {
+    contentWrapperElemRef.current!.style.transform = `translateX(${translateYVal}px)`;
+    if (isHideButton(contentWrapperElemRef.current!, containerElemRef.current!, translateYVal)) {
+      setIsButtonHide(true);
+    };
+  }, [translateYVal, isHideButton])
+
   return (
-    <div>
-      {/* <IntroductionCard/> */}
-      {/* <MoonshotCard /> */}
-      {/* <Web3Card/> */}
-      <RightButton />
+    <div ref={containerElemRef} className="relative w-[calc(416px*3)] h-[max-content] left-[124px] top-[185px] overflow-x-hidden overflow-y-hidden">
+      <div className="w-[1352px] flex flex-row gap-[52px]" ref={contentWrapperElemRef}>
+        <IntroductionCard />
+        <MoonshotCard />
+        <Web3Card />
+      </div>
+      <div ref={buttonWrapperElemRef} className="absolute right-[0px] top-[115px]" onClick={handleRightButtonClick}>
+        <RightButton isDisabled={isButtonHide} />
+      </div>
     </div>
   )
 }
